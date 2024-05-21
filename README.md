3dd9cb9fb7823f0b635ab090ceac26118aba072d
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-05-21 20:40:10 -0300

    feat: documentation

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# minigame
+# Insider-E: Minigame
+
+Este repositório contém o código-fonte e a documentação para um aplicativo de quiz desenvolvido para a Insider-E, focado em corridas de Fórmula E. O aplicativo permite aos usuários responder perguntas sobre Fórmula E, obter informações sobre a última corrida, visualizar detalhes da próxima corrida e ver o ranking das equipes.
+
+## Funcionalidades
+
+- **Quiz Interativo:** Responda perguntas sobre Fórmula E e acumule pontos.
+- **Informações da Última Corrida:** Veja detalhes sobre a última corrida de Fórmula E, incluindo data, ganhador e local.
+- **Informações da Próxima Corrida:** Obtenha informações sobre a próxima corrida, incluindo data, hora, local e equipes participantes.
+- **Ranking de Equipes:** Veja o ranking das equipes baseado no número de vitórias.
+
+## Arquivos
+
+- `app.py`: Código principal do aplicativo.
+- `perguntas.json`: Arquivo JSON contendo as perguntas e respostas do quiz.
+- `infoUltimaCorrida.json`: Arquivo JSON contendo informações sobre a última corrida.
+- `infoProxCorrida.json`: Arquivo JSON contendo informações sobre a próxima corrida.
+- `equipes.json`: Arquivo JSON contendo informações sobre as equipes e seus rankings.
+
+## Requisitos
+
+- Python 3.x
+- Biblioteca `textwrap`
+- Biblioteca `json`
+
+## Instalação
+
+1. Clone este repositório:
+   ```bash
+   git clone https://github.com/Insider-E/minigame
+2. Navegue até o diretório do projeto:
+   ```bash
+   cd minigame-insider-e
+## Utilização
+
+1. Certifique-se de que os arquivos `perguntas.json`, `infoUltimaCorrida.json`, `infoProxCorrida.json` e `equipes.json` estejam no mesmo diretório que `app.py`.
+
+2. Execute o programa:
+
+    ```python
+    py app.py
+3. Siga as instruções no terminal para interagir com o aplicativo.
+
+## Exemplo de execução
+    
+    -------------------------------------------------------------
+    ----------------SEJA BEM VINDO(A) À INSIDER-E----------------
+    -------------------------------------------------------------
+
+    DIGITE SEU NOME: João
+
+    | ----------------------------------------------- |
+    |         CÓDIGO        |         OPÇÃO           |
+    | ----------------------------------------------- |
+    |            1          |          QUIZ           |
+    |            2          | INFO. DA ÚLTIMA CORRIDA |
+    |            3          | INFO. DA PRÓX. CORRIDA  |
+    |            4          |   RANKING DE EQUIPES    |
+    | ----------------------------------------------- |
+
+    João, digite o código referente à opção que deseja:
+
+## Integrantes:
+
+- David Murillo de Oliveira Soares (RM 559078)
+- Davi dos Reis Garcia (RM 556741)
+- Yasmin Gonçalves Coelho (RM 559147)
+- Yasmin Naomi Minemoto (RM 559154)
+
+
+## Contribuições
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir issues para sugestões de melhorias ou para enviar pull requests.
+
+---
+
+Este aplicativo é uma ferramenta interativa e informativa para os fãs da Fórmula E, ajudando-os a testar seus conhecimentos e se manterem atualizados sobre as corridas. Sinta-se à vontade para explorar, utilizar e melhorar este projeto
--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import textwrap
 import json
 
+#Função para o quiz
 def quiz():
+    #Abre e lê o arquivo JSON contendo as perguntas e respostas
     f = open('perguntas.json')
     perguntas = json.load(f)
     
+    #Pontos acumulados a cada resposta correta
     pontos = 0
         
+    #textwrap.dedent = remove a identação do texto, para não ficar bagunçado na exibição no terminal
     print(textwrap.dedent('''
     -------------------------------------------------------------
     ---------------BEM-VINDO(A) AO QUIZ INSIDER-E!---------------
@@ -16,12 +20,17 @@ def quiz():
     - Cada alternativa correta vale 5 PONTOS.
     '''))
         
+    #Percorre o array de perguntas, exibindo e pegando o valor de entrada do usuário como resposta
     for pergunta in perguntas['perguntasQuiz']:
+        #' '.join(map(str, pergunta['options'])) = passa por todos os elementos de 'pergunta['options']' usando o map e
+        #concatena todos em uma string, separando com espaço
         resposta = input(f'{pergunta['question']}\n{' '.join(map(str, pergunta['options']))}\n')
-            
+           
+        #Verifica se a resposta digitada está correta e acumula 5 pontos para o usuário 
         if resposta.upper() == pergunta['answer'].upper():
             pontos += 5
         
+    #Exibe a pontuação
     print(textwrap.dedent(f'''
     -------------------------------------------------------------
     ---------------SUA PONTUAÇÃO FOI DE {pontos} PONTOS----------------
@@ -30,10 +39,13 @@ def quiz():
     
     return pontos
         
+#Função para exibir informações sobre a última corrida
 def infoUltimaCorrida():
+    #Abre e lê o arquivo JSON contendo as informações
     f = open('infoUltimaCorrida.json')
     infoUltimaCorrida = json.load(f)['info']
     
+    #textwrap.dedent = remove a identação do texto, para não ficar bagunçado na exibição no terminal
     print(textwrap.dedent(f'''
     -------------------------------------------------------------
     ------------INFORMAÇÕES SOBRE A ÚLTIMA CORRIDA---------------
@@ -49,11 +61,16 @@ def infoUltimaCorrida():
     
     -------------------------------------------------------------
     '''))
-    
+  
+#Função para exibir informações sobre a próxima corrida  
 def infoProxCorrida():
+    #Abre e lê o arquivo JSON contendo as informações
     f = open('infoProxCorrida.json')
     infoProxCorrida = json.load(f)['info']
     
+    #textwrap.dedent = remove a identação do texto, para não ficar bagunçado na exibição no terminal
+    #', '.join(map(str, infoProxCorrida['teams'])) = passa por todos os elementos de 'infoProxCorrida['teams']' usando o map e
+    #concatena todos em uma string separando por vírgula
     print(textwrap.dedent(f'''
     -------------------------------------------------------------
     ------------INFORMAÇÕES SOBRE A PRÓXIMA CORRIDA--------------
@@ -72,20 +89,26 @@ def infoProxCorrida():
     -------------------------------------------------------------
     '''))
     
+#Função para exibir informações sobre a próxima corrida 
 def rankingEquipes():
+    #Abre e lê o arquivo JSON contendo as equipes
     f = open('equipes.json')
     equipes = json.load(f)['teams']
     
+    #textwrap.dedent = remove a identação do texto, para não ficar bagunçado na exibição no terminal
     print(textwrap.dedent(f'''
     -------------------------------------------------------------
     ---------------------RANKING DE EQUIPES----------------------
     -------------------------------------------------------------
     '''))
     
+    #Ordena as equipes por ordem descrescente considerando o número de vitórias (reverse = True)
     equipesOrdenadas = sorted(equipes, key=lambda equipe: equipe['victories'], reverse=True)
     
+    #Contador para definir a posição das equipes
     i = 1
     
+    #Exibe cada equipe por ordem descrescente considerando o número de vitórias e suas respectivas posições no ranking
     for equipe in equipesOrdenadas:
         print(f'{i}° - {equipe['name']} | {equipe['victories']} vitórias')
         i += 1
@@ -101,6 +124,7 @@ print('''
 nome = input('DIGITE SEU NOME: ')
 
 while True:
+    #textwrap.dedent = remove a identação do texto, para não ficar bagunçado na exibição no terminal
     opcao = input(textwrap.dedent(f'''
 
     | ----------------------------------------------- |
@@ -115,28 +139,32 @@ while True:
     {nome}, digite o código referente à opção que deseja: 
     '''))
 
+    #Verifica a opção que o usuário digitou e leva para sua respectiva função,
+    #caso a opção for inválida o usuário é informado
+    #Em cada opção há uma pergunta para saber se o usuário quer continuar, se for digitado 2 (NÃO),
+    #é executado um break no loop e o programa para
     if opcao == '1':
         quiz()
-        cont = int(input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n'))
-        if cont == 2:
+        cont = input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n')
+        if cont == '2':
             break
         
     elif opcao == '2':
         infoUltimaCorrida()
-        cont = int(input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n'))
-        if cont == 2:
+        cont = input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n')
+        if cont == '2':
             break
         
     elif opcao == '3':
         infoProxCorrida()
-        cont = int(input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n'))
-        if cont == 2:
+        cont = input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n')
+        if cont == '2':
             break
         
     elif opcao == '4':
         rankingEquipes()
-        cont = int(input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n'))
-        if cont == 2:
+        cont = input('Deseja continuar?\n 1 - SIM\n 2 - NÃO\n')
+        if cont == '2':
             break
         
     else:
